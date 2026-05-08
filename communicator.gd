314fb96a9195edead2dd380d51ae7cbe0a56018d
TetroTheo <theotoastman@gmail.com>
2026-05-08 00:15:41 -0700

    you can now add rests, change the note duration, and create chords with control buttons. triplets and dotted notes should be relatively simple to add. however! there is a slight bug where you can overload the duration of a measure, because it does not check for that before you add in notes. also, it might be useful to add syncopation; however, it is unclear how this could be accomplished. perhaps it could be stored in the following npkgn.

--- communicator.gd
+++ communicator.gd
@@ -1,7 +1,37 @@
 extends Node
 
+var duration := 1.0
+var chord_notes : Array[Note] = []
+var chord_creation := false
+
 func key_pressed(note):
+	if chord_creation:
+		chord_notes.append(note)
+		return
 	var npkg = NotePackage.new()
-	npkg.duration = 1.0
+	npkg.duration = duration
 	npkg.notes.append(note)
 	$Sheet.add_note_pkg(npkg)
+
+
+func on_duration_changed(index: int) -> void:
+	duration = 4 * pow(0.5, index)
+
+
+func on_rest_pressed() -> void:
+	var npkg = NotePackage.new()
+	npkg.duration = duration
+	$Sheet.add_note_pkg(npkg)
+
+
+func on_chord_toggled(toggled_on: bool) -> void:
+	chord_creation = toggled_on
+	if not chord_creation:
+		if chord_notes:
+			var npkg = NotePackage.new()
+			npkg.duration = duration
+			for note in chord_notes:
+				# make sure that notes do not repeat strings later
+				npkg.notes.append(note)
+			$Sheet.add_note_pkg(npkg)
+			chord_notes.clear()
