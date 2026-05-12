5cd2dbb3785500396bb16b1fcbe31faec775dc2a
TetroTheo <theotoastman@gmail.com>
2026-05-12 11:46:43 -0700

    made a few small changs. fret zero is now much smaller than fret one, when fret scaling is enabled. also, the exact value of the scaling factor has been found!

--- test-fretboard.gd
+++ test-fretboard.gd
@@ -9,6 +9,8 @@ var LEFT_HANDED := false
 var BASS_MODE := false
 var FRET_NUMBER := 24
 var SCALE_FRETS := true
+## we derived the scaling constant!
+const SCALE_FRET_FACTOR : float = (1 - (2 ** (-1/12.0))) ** -1
 
 ## instantiates keys in the fretboard
 func _ready() -> void:
@@ -33,15 +35,18 @@ func create_keys():
 		var keys : Array[Key] = []
 		## determine the length of each fret (may change)
 		var fret_length : float
-		var remaining_length : float = 2400
+		var remaining_length : float = 3_000
 		if SCALE_FRETS:
-			fret_length = remaining_length / 17.817
+			fret_length = remaining_length / SCALE_FRET_FACTOR
 		else: ## constant length
 			fret_length = 42
 		## add one to include fret zero
 		for j in range(FRET_NUMBER + 1):
 			var key = Key.new()
-			key.set_length(fret_length)
+			if SCALE_FRETS and not j:
+				key.set_length(40)
+			else:
+				key.set_length(fret_length)
 			key.set_note(Note.new(j, pitch, i))
 			## if we are running this scene on its own, then this shouldn't run!
 			if get_parent().name == "Communicator":
@@ -52,8 +57,8 @@ func create_keys():
 			## TODO: find out where this number came from!
 			if SCALE_FRETS:
 				remaining_length -= fret_length
-				fret_length = remaining_length / 17.817
-				print(fret_length)
+				fret_length = remaining_length / SCALE_FRET_FACTOR
+				print(fret_length, " ", SCALE_FRET_FACTOR)
 		## now that the keys are in an array, we flip them like this:
 		if LEFT_HANDED:
 			keys.reverse()
